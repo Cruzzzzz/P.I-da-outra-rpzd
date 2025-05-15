187787c1bc077576e09bd6cb76f577512c778c81
Arthur da Cruz <cruzz16.mr@gmail.com>
2025-05-15 16:03:06 -0300

    Revert "Finalmente deu certooooo"
    This reverts commit 6cbe70f2178309df9c951f6c11212a768b26e746.

--- My project/Assets/Scripts/MovementPlayer.cs
+++ My project/Assets/Scripts/MovementPlayer.cs
@@ -6,16 +6,13 @@ public class MovementPlayer : MonoBehaviour
     [SerializeField] private float runSpeed = 10f; // Corrida
     [SerializeField] private float speed; // Velocidade atual do player
     public Rigidbody2D rb;
-    [SerializeField] Animator anim;
-
+   
 
     Vector2 movement; 
     private void Start()
     {
         rb.gravityScale = 0;
         speed = moveSpeed;
-        rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
     }
     void Update()
     {
@@ -31,9 +28,7 @@ public class MovementPlayer : MonoBehaviour
         {
             speed = moveSpeed;
         }
-        anim.SetFloat("Horizontal", movement.x);
-        anim.SetFloat("Vertical", movement.y);
-        anim.SetFloat("Speed", movement.sqrMagnitude);
+
     }
 
     void FixedUpdate()
